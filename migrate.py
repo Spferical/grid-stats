6e4a24a4c609b3b317768664e83bea9642f5ed13
Matthew Pfeiffer <spferical@gmail.com>
2016-09-13 19:15:47 -0400

    Influxdb migration with retry on error

--- migrate.py
+++ migrate.py
@@ -1,10 +1,13 @@
 import database as db
 from influxdb import InfluxDBClient
+from time import sleep
 s = db.Session()
 users = s.query(db.User)
 c = InfluxDBClient()
 c.switch_database('grid')
 logs = s.query(db.UserLog).yield_per(100)
+count = logs.count()
+print(count, "logs")
 
 usernames = {}
 for user in s.query(db.User):
@@ -12,7 +15,9 @@ for user in s.query(db.User):
 
 points = []
 
+i=0
 for log in logs:
+    i += 1
     points.append({
         "tags": {"username": usernames[log.user_id]},
         "time": log.time,
@@ -25,6 +30,15 @@ for log in logs:
             "bank": log.bank}})
 
     if len(points) >= 100:
-        c.write_points(points)
+        worked = False
+        while not worked:
+            try:
+                c.write_points(points)
+                worked = True
+            except:
+                print("Exception! Waiting a minute.")
+                sleep(60)
         points = []
+        print(i, '/', count)
+        sleep(1)
 c.write_points(points)
