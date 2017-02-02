7bc7d86107989b6ca2d830eb82938ac3b5d501e9
Matthew Pfeiffer <spferical@gmail.com>
2017-02-01 22:05:58 -0500

    Rip out dead code

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 from bs4 import BeautifulSoup
 import re
 import argparse
-import datetime
 import database
 try:  # Python 3
     from urllib.request import urlopen
@@ -83,55 +82,6 @@ def update_database():
     data = get_ranks_table()
     players = [parse_player(row) for row in data]
     database.write_player_data(players)
-
-
-def create_database():
-    client = InfluxDBClient()
-    client.create_database('grid')
-
-
-def set_foregroundcolor(ax, color):
-    '''For the specified axes, sets the color of the frame, major ticks,
-    tick labels, axis labels, title and legend
-    From https://gist.github.com/jasonmc/1160951
-    '''
-    for tl in ax.get_xticklines() + ax.get_yticklines():
-        tl.set_color(color)
-    for spine in ax.spines:
-        ax.spines[spine].set_edgecolor(color)
-    for tick in ax.xaxis.get_major_ticks():
-        tick.label1.set_color(color)
-    for tick in ax.yaxis.get_major_ticks():
-        tick.label1.set_color(color)
-    ax.axes.xaxis.label.set_color(color)
-    ax.axes.yaxis.label.set_color(color)
-    ax.axes.xaxis.get_offset_text().set_color(color)
-    ax.axes.yaxis.get_offset_text().set_color(color)
-    ax.axes.title.set_color(color)
-    lh = ax.get_legend()
-    if lh is not None:
-        lh.get_title().set_color(color)
-        lh.legendPatch.set_edgecolor('none')
-        labels = lh.get_texts()
-        for lab in labels:
-            lab.set_color(color)
-    for tl in ax.get_xticklabels():
-        tl.set_color(color)
-    for tl in ax.get_yticklabels():
-        tl.set_color(color)
-
-
-def set_backgroundcolor(ax, color):
-    '''Sets the background color of the current axes (and legend).
-         Use 'None' (with quotes) for transparent. To get transparent
-         background on saved figures, use:
-         pp.savefig("fig1.svg", transparent=True)
-         From https://gist.github.com/jasonmc/1160951
-     '''
-    ax.patch.set_facecolor(color)
-    lh = ax.get_legend()
-    if lh is not None:
-        lh.legendPatch.set_facecolor(color)
 
 
 def main():
